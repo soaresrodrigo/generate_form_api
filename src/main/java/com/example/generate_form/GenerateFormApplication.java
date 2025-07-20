71c5c5c990c38e6c1fdaea70135aeb47ba8a5fb3
Rodrigo Soares <rodrigo@obi.tec.br>
2025-07-20 16:35:37 -0300

    :tada: Initial commit

---
+++ src/main/java/com/example/generate_form/GenerateFormApplication.java
@@ -0,0 +1,13 @@
+package com.example.generate_form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/generate_form/GenerateFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.generate_form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
